b6873706c28fb9c493f4bfb6a229a5b574321499
partner.Illia.Ostrovskyi@philips.com <partner.Illia.Ostrovskyi@philips.com>
2025-05-23 16:55:30 +0300

    init commit

---
+++ Simple Console App/Program.cs
@@ -0,0 +1,9 @@
+﻿using Simple_Console_App;
+
+var user = new User()
+{
+    Id = 1,
+    Name = "DefaultUser"
+};
+    
+Console.WriteLine($"User id: {user.Id} User Name: {user.Name}");
---
+++ Simple Console App/User.cs
@@ -0,0 +1,7 @@
+namespace Simple_Console_App;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
